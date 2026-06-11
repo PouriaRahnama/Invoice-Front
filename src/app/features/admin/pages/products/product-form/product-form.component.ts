c4799e5b20db976ceddf9cf5edf757115d23a5d7
PouriaRahnama <pouria.rahnama78@gmail.com>
2026-06-12 01:30:32 +0330

    add create and edit product

--- src/app/features/admin/pages/products/product-form/product-form.component.ts
+++ src/app/features/admin/pages/products/product-form/product-form.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ProductService } from '../../../../../core/services/product.service';
-import { Router, RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { finalize } from 'rxjs';
+import { finalize, Observable } from 'rxjs';
 import { ToastrService } from 'ngx-toastr';
+import { ApiResponse } from '../../../../../core/models/api-response.model';
+import { ApiService } from '../../../../../core/services/api.service';
+import { FieldErrorComponent } from "../../../../../shared/validators/field-error/field-error.component";
 
 @Component({
   selector: 'app-product-form',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink,CommonModule],
+  imports: [ReactiveFormsModule, RouterLink, CommonModule, FieldErrorComponent],
   templateUrl: './product-form.component.html',
   styleUrl: './product-form.component.css'
 })
@@ -20,19 +23,61 @@ export class ProductFormComponent implements OnInit {
   selectedFile: File | null = null;
   imagePreview: string | null = null;
   selectedFileName: string = '';
+  productId: string | null = null;
+  isEditMode: boolean = false;
+  currentImageUrl: string | null = null; 
 
   constructor(private fb: FormBuilder,
     private productService:ProductService,
+    private apiService:ApiService,
     private router: Router,
-    private toastr: ToastrService) {
+    private toastr: ToastrService,
+    private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
+
+    this.initForm();
+
+    const idParam = this.route.snapshot.paramMap.get('id');
+    if (idParam) {
+      this.isEditMode = true;
+      this.productId = idParam;
+      this.loadProductForEdit();
+    }
+  }
+
+  private initForm(): void {
     this.productForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(3)]],
       price: [0, [Validators.required, Validators.min(1)]],
-      quantity: [0, [Validators.required, Validators.min(1)]]
+      quantity: [0, [Validators.required, Validators.min(1)]] 
     });
+  }
+
+  private loadProductForEdit(): void {
+    if (!this.productId) return;
+
+    this.isSubmitting = true;
+    this.productService.getByProductId(this.productId)
+      .pipe(finalize(() => this.isSubmitting = false))
+      .subscribe({
+        next: (res) => {       
+          this.productForm.patchValue({
+            name: res.data.name,
+            price: res.data.price,
+            quantity: res.data.quantity
+          });
+
+          this.currentImageUrl = res.data.imagePath || null;
+          this.imagePreview = this.apiService.baseUrlForImages + this.currentImageUrl;
+        },
+        error: (err) => {
+          const message = err?.error?.message || 'خطا در بارگذاری اطلاعات محصول.';
+          this.toastr.error(message, 'خطا');
+          this.router.navigate(['/admin/products']);
+        }
+      });
   }
 
   onFileSelected(event: Event): void {
@@ -58,20 +103,25 @@ export class ProductFormComponent implements OnInit {
   }
 
   onSubmit(): void {
-
     if (this.productForm.valid) {
       const formData = new FormData();
       formData.append('Name', this.productForm.value.name.toString());
       formData.append('Price', this.productForm.value.price);
       formData.append('Quantity', this.productForm.value.quantity);
-      if (this.selectedFile) {
-        formData.append('Image', this.selectedFile);
-      }
-
+      if (this.selectedFile) {formData.append('Image', this.selectedFile);}
       this.isSubmitting = true;
 
-      this.productService.createProduct(formData)
-        .pipe(finalize(() => this.isSubmitting = false))
+      let request : Observable<ApiResponse<any>>;
+      if(this.isEditMode && this.productId)
+      {
+        formData.append('ProductId', this.productId.toString());
+        request = this.productService.UpdateProduct(formData)
+      }
+      else{
+        request = this.productService.createProduct(formData)
+      }
+
+      request.pipe(finalize(() => this.isSubmitting = false))
         .subscribe({
         next: (res) => {
           this.toastr.success(res.message || 'محصول با موفقیت ایجاد شد.', 'موفق');
@@ -81,8 +131,10 @@ export class ProductFormComponent implements OnInit {
            const message = err?.error?.message || 'خطایی در ایجاد محصول رخ داده است.';
            this.toastr.error(message, 'خطا');
         }
-      });   
-    } else {
+      });  
+
+    } 
+    else {
       this.productForm.markAllAsTouched(); 
     }
   }
