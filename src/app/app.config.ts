e0fa3c97ddddb16ee33eaf63dce598e0c5305336
PouriaRahnama <pouria.rahnama78@gmail.com>
2026-06-12 15:40:05 +0330

    Fix app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,21 +2,24 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),  
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     provideAnimations(),
     provideToastr({
-    positionClass: 'toast-top-left',
-    timeOut: 3000,
-    closeButton: true,
-    progressBar: true,
-    preventDuplicates: false
-  })
+      positionClass: 'toast-top-left',
+      timeOut: 3000,
+      closeButton: true,
+      progressBar: true,
+      preventDuplicates: true, 
+      maxOpened: 3
+    }),
   ],
 };
