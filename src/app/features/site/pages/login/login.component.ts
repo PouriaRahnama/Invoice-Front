9e52115f13eb017b2f21c54b6b211c6324f5becb
PouriaRahnama <pouria.rahnama78@gmail.com>
2026-06-12 22:15:23 +0330

    add login component

---
+++ src/app/features/site/pages/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [RouterLink, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
